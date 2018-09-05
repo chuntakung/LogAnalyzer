52b6cbb6b817e97259a3933f1b66be5fac7050e4
chun-ta.kung@wdc.com <chun-ta.kung@wdc.com>
2018-09-05 21:36:10 +0800

    [FEATURE] viz of rtw disconnections in matplotpy and plotly

--- utils.py
+++ utils.py
@@ -8,7 +8,13 @@ DATE        INITIAL     CONTENTS
 ===============================================================================
 20180822    ck          initial version
 20180903    ck          added extraction of RTW layer disconnection events
+20180907    ck          plotting of rtw disconnection using plotly and matplotpy
 """
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import matplotlib.pyplot as plt
 
 #==============================================================================
 # function to extract message content
@@ -33,6 +39,9 @@ class MsgStats:
     MSG_COL = 4
 
     def __init__(self, msgls):
+
+        plotly.tools.set_credentials_file(username='chuntakung', api_key='bEecqEVf7mXmdWQJr75g')
+
         # statistical counts
         self.disconnection_events = 0
         self.good_disconnection_events = 0
@@ -43,11 +52,10 @@ class MsgStats:
         self.msg_list.sort(key=lambda x: x[0])
 
         # disconnection periods
-        self.RTW_disconnections = []
+        self.rtw_connection_evt = []
         self.wpas_disconnections = []
         self.WSM_disconnections = []
         self.restSDK_disconnections = []
-        self.RTW_connections = []
         self.parse_disconnections()
 
     def count_disconnection(self):
@@ -66,16 +74,64 @@ class MsgStats:
                 ("cfg80211_rtw_disconnect(wlan0)" in obj[MsgStats.MSG_COL]) or \
                 ("rtw_cfg80211_indicate_disconnect" in obj[MsgStats.MSG_COL]) or \
                 ("Start to Disconnect" in obj[MsgStats.MSG_COL]):
-                print("{0} ==> {1}".format(i, obj[MsgStats.MSG_COL]))
-                self.RTW_disconnections.append([i,obj[MsgStats.DATETIME_COL]])
+                print("dbg: {0} ==> {1}".format(i, obj[MsgStats.MSG_COL]))
+                self.rtw_connection_evt.append({'index':i, 'connection_status':0})
             elif ("cfg80211_rtw_connect(wlan0)" in obj[MsgStats.MSG_COL]) or \
                 ("rtw_cfg80211_indicate_connect" in obj[MsgStats.MSG_COL]) or \
                 ("Start to Connection" in obj[MsgStats.MSG_COL]):
-                print("{0} ==> {1}".format(i, obj[MsgStats.MSG_COL]))
-                self.RTW_connections.append([i,obj[MsgStats.DATETIME_COL]])
+                print("dbg: {0} ==> {1}".format(i, obj[MsgStats.MSG_COL]))
+                self.rtw_connection_evt.append({'index':i, 'connection_status':1})
+
+        self.rtw_connection_evt.sort(key=lambda x: x['index'])
 
     def print_msg(self):
         print(self.msg_list)
 
+    def prepare_rtw_disconnection_xy(self):
+        x = [ x[0] for x in self.msg_list ]
+        y = None
+        for i, item in enumerate(self.rtw_connection_evt):
+            print("dbg: {0} {1}".format(i, item))
+            # interpolation
+            if y != None:
+                y = y + [y[-1]]*(item['index']-len(y))
+
+            if item['connection_status'] == 0:
+                # initialize
+                if y == None:
+                    y = [1]*item['index']
+
+                # appened latest status
+                y.append(0)
+            else:
+                # initialize
+                if y == None:
+                    y = [0]*item['index'] # TODO: check upper later message to determine
+
+                # appened latest status
+                y.append(1)
+
+        # padding to end
+        y = y + [y[-1]]*(len(x)-len(y))
+        return x,y
+
+
+    def plot_disconnection(self):
+        x,y = self.prepare_rtw_disconnection_xy()
+
+        # plotting
+        plt.plot(x, y,'r')
+        plt.ylim(-0.5, 1.5)
+        for i, item in enumerate(self.rtw_connection_evt):
+            if item['connection_status'] == 0:
+                plt.annotate('disconnected', xy=(x[item['index']],0), xytext=(x[item['index']], 1.2))
+
+        plt.show() # does not matter when in interactive mode
+
+    def plot_disconnection_plotly(self):
+        x,y = self.prepare_rtw_disconnection_xy()
+        data = [go.Scatter(x=x, y=y)]
+        fig = go.Figure(data=data)
+        py.plot(fig)
 
 
